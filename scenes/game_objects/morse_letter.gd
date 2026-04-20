22938f2029ebd963bd5a4808a952c39c41e32e14
Joshua Elliott <dottedpaper@proton.me>
2026-04-20 15:46:40 -0700

    bg music, rebalance audio, attenuate on beeps

--- scenes/game_objects/morse_letter.gd
+++ scenes/game_objects/morse_letter.gd
@@ -35,6 +35,9 @@ const ALPHA_TO_MORSE: Dictionary[String, String] = {
 @export var letter: String = "A"
 @export var dit_stream: AudioStream
 @export var dah_stream: AudioStream
+@export var music_atten_factor: float
+@export var music_atten_time_in: float
+@export var music_atten_time_out: float
 
 @onready var mouse_area: Area2D = %MouseArea
 @onready var morse_audio_player: AudioStreamPlayer = %MorseAudioPlayer
@@ -50,6 +53,15 @@ func _ready() -> void:
 func play() -> void:
     playing = true
     played.emit()
+    
+    # music down
+    var music_bus_idx = AudioServer.get_bus_index("Music")
+    var music_bus_volume = AudioServer.get_bus_volume_linear(music_bus_idx)
+    var set_music_bus_volume = func(vol):
+        AudioServer.set_bus_volume_linear(music_bus_idx, vol)
+    var in_tween = get_tree().create_tween()
+    in_tween.tween_method(set_music_bus_volume, music_bus_volume, music_bus_volume*music_atten_factor, music_atten_time_in)
+    
     var morse := ALPHA_TO_MORSE[letter]
 
     for c in morse:
@@ -66,6 +78,10 @@ func play() -> void:
     await get_tree().create_timer(0.5).timeout
     light_sprite.play("default")
     playing = false
+    
+    # music up
+    var out_tween = get_tree().create_tween()
+    out_tween.tween_method(set_music_bus_volume, music_bus_volume*music_atten_factor, music_bus_volume, music_atten_time_out)
 
 
 func on_mouse_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
